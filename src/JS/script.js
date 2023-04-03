4b4ce5973e4aaf964c843b709a82a39e84beef83
OUSSAMA HRITA <hrita.oussama@ensam-casa.ma>
2023-04-03 00:45:18 +0000

    Add sun and background sun

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
-
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import stars from '../Material/Background.jpg';
+import Sun from '../Material/sun.jpg';
 const renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -10,8 +12,23 @@ const camera=new THREE.PerspectiveCamera(
     0.1,
     1000
 );
+const orbit=new OrbitControls(camera,renderer.domElement);
 const axesHelper=new THREE.AxesHelper(5)
 scene.add(axesHelper);
-camera.position.z=5;
+camera.position.set(2,2,20);
+/*Stars Background*/
+const textureLoader=new THREE.TextureLoader();
+scene.background=textureLoader.load(stars);
+/* */
+/*SUN*/
+const sunGeometry =new THREE.SphereGeometry(4);
+const sunMaterial=new THREE.MeshBasicMaterial({map:textureLoader.load(Sun)});
+const sun=new THREE.Mesh(sunGeometry,sunMaterial);
+scene.add(sun);
+/**/ 
+orbit.update();
 renderer.render(scene,camera);
-
+function animate(time){
+    renderer.render(scene,camera);
+}
+renderer.setAnimationLoop(animate);
